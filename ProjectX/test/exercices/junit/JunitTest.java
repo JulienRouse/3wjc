a25c313497c05624594adc338f5565bfff67d32d
JulienRouse <julien.rouse@gmail.com>
2017-04-24 11:45:07 +0200

    Add example of Matchers for tests. Updates JUnit version in pom.xml

---
+++ ProjectX/test/exercices/junit/JunitTest.java
@@ -0,0 +1,41 @@
+package exercices.junit;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+	//core
+	@Test
+	public void testMatcherAnything() {
+		assertThat("hello world", anything("always match"));
+		assertThat(42, anything("always match"));
+	}
+
+	@Test
+	public void testMatcherIs(){
+		//is(42)==is(equalTo(42))
+		assertThat(42, is(42));
+		assertThat("hello", is("hello"));
+	}
+
+	//logical
+	@Test
+	public void testMatcherAllOf(){
+		//court circuit comme &&
+		 assertThat("abc", allOf(containsString("a"), (containsString("b"))));
+	}
+	
+	@Test 
+	public void testMatcherAnyOf(){
+		//court circuit comme ||
+		assertThat("abc", anyOf(containsString("z"), containsString("a")));
+	}
+	
+	@Test
+	public void testMatcherNot(){
+		assertThat(42, not(is(0)));
+	}
+}

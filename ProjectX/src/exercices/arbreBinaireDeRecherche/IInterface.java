bcf149180f43d8db90e324e1e3f49ebff8102385
JulienRouse <julien.rouse@gmail.com>
2017-04-24 17:47:50 +0200

    Ajout de commentaires

--- ProjectX/src/exercices/arbreBinaireDeRecherche/IInterface.java
+++ ProjectX/src/exercices/arbreBinaireDeRecherche/IInterface.java
@@ -1,5 +1,7 @@
 package exercices.arbreBinaireDeRecherche;
 
+
+//Le nom de l'interface ne va pas du tout, trouver qualque chose qui exprime mieux sa vocation
 @FunctionalInterface
 public interface IInterface {
 	public void func(int i, Noeud n);
